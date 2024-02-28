832e3e5699cab283961200da48599c0c3fafd4db
Ebxxx <itsebs758@gmail.com>
2024-02-28 11:50:32 +0800

    Update
    Removed the css internal style replaced by the external css style. And i add some documentation in the code.

--- registrationform.php
+++ registrationform.php
@@ -1,29 +1,27 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <!-- title of the page-->
     <title>REGISTER</title>
+       <!--External bootsrap file-->
     <link rel="Stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    
-    <style>
-        .form-wrapper {
-            background-color:   rgb(57, 144, 225);
-            box-shadow: 0 6px 10px rgb(0, 0, 0, 0.741);
-            padding: 20px;
-            border-radius: 20px;
-            color: white;
-        }
-    </style>
+    <!-- External css file -->
+    <link rel="Stylesheet" href="stylesheet.css">
 </head>
 <body>
     <div class="container">
         <div class="content">
+            <!-- column set to he center -->
             <div class="col-md-6 offset-md-3">
+                <!--form wrapper with styling -->
                 <div class="form-wrapper mt-5">
+                    <!--Title of the form-->
                     <h2 class="text-center mb-4">REGISTRATION</h2>
+                    <!-- Display error message if user input exists -->
                     <?php if (isset($_GET['error'])) { ?>
                         <div class="alert alert-danger"><?php echo $_GET['error']; ?></div>
                     <?php } ?>
-
+                        <!--Registration form with varous input lentgh-->
                     <form action="register.php" method="POST">
                         <div class="form-group">
                             <label for="username">User name:</label>
@@ -48,13 +46,14 @@
                         <div class="form-group">
                             <label for="Email">Email:</label>
                             <input type="email" class="form-control" name="Email">
-                        </div>
+                        </div>  
                         <div class="form-group">
                             <label for="Status">Status:</label>
                             <input type="text" class="form-control" name="Status">
                         </div><br>
                         <button type="submit" class="btn btn-primary">Submit</button>
                         <br><br>
+                        <!-- link to loginform -->
                         <a href="loginform.php">Click to Login</a>
                     </form>
                 </div>
